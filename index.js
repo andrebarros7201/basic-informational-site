24fc38f130f7946ecb570f3f41c05ca4335c1109
Andre Barros <andrebarros7201@gmail.com>
2024-06-29 17:24:36 +0100

    link between pages

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const http = require("http");
-const fs = require("fs");
 const url = require("url");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-    
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+    if (err) throw err;
+    return data;
 });
+
+http.createServer( (req, res) => {
+        const q = url.parse(req.url, true);
+        let filename = "";
+        if (q.pathname === "/") {
+            filename = "." + "/index.html";
+        } else {
+            filename = "." + q.pathname;
+        }
+
+        fs.readFile(filename, function (err, data) {
+            if (err) {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                res.write(page404);
+                return res.end();
+            } else {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write(data);
+                return res.end();
+            }
+        });
+    })
+    .listen(8080);
