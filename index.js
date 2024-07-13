32f03768e499e688b23ba19dd4efe114eeaee08a
Andre Barros <andrebarros7201@gmail.com>
2024-07-13 17:10:59 +0100

    rewrite using express

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const http = require("http");
-const url = require("url");
+const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
+const app = express();
+const port = 8080;
+
+// Load 404 page content
 const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
     if (err) throw err;
     return data;
 });
 
-http.createServer( (req, res) => {
-        const q = url.parse(req.url, true);
-        let filename = "";
-        if (q.pathname === "/") {
-            filename = "." + "/index.html";
-        } else {
-            filename = "." + q.pathname;
-        }
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
 
-        fs.readFile(filename, function (err, data) {
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/html" });
-                res.write(page404);
-                return res.end();
-            } else {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.write(data);
-                return res.end();
-            }
-        });
-    })
-    .listen(8080);
+// Handle root route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "about.html"));
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, "contact-me.html"));
+})
+
+// Handle all other routes
+app.use((req, res, next) => {
+    res.status(404).send(page404);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
